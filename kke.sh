f86b99597facd65cad61f4e5fb1dc5b21683bde1
Waldemar <vladimir@bondar.ru>
2023-08-22 22:34:27 +0300

    14 commit

--- kke.sh
+++ kke.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+# Function to display commands
+exe() { echo "\$ $@" ; "$@" ; }
 
+cd ~/kke
 echo Initial thor password: mjolnir123
-sudo dnf install ansible -y
+exe sudo dnf install ansible -y
 ansible-playbook -i inventory thor.yml
+cd
