23af67836c34178357940bb38913b70313b91c97
vladimirbondar <115801580+vladimirbondar@users.noreply.github.com>
2024-06-23 22:43:54 +0300

    Update kke.sh

--- kke.sh
+++ kke.sh
@@ -2,10 +2,26 @@
 # Function to display commands
 exe() { echo "\$ $@" ; "$@" ; }
 
+# Цвета и яркости текста
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+BOLD_RED='\033[1;31m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_BLUE='\033[1;34m'
+RESET='\033[0m'
+
+# Примеры использования
+echo -e "${BOLD_GREEN}Этот текст будет ярко-зеленым${RESET}"
+echo -e "${YELLOW}Этот текст будет желтым${RESET}"
+echo -e "${BOLD_BLUE}Этот текст будет ярко-синим${RESET}"
+
 cd ~/kke
 echo
 echo Initial thor password:
-echo ">>>>> mjolnir123 <<<<<"
+echo -e ">>>>> ${BOLD_GREEN}mjolnir123${RESET} <<<<<"
 echo ">>>>> ^^^^^^^^^^ <<<<<"
 echo
 mjolnir123 | sudo su
